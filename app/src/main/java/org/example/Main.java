7f8c5739e33d997b75c8d4fa8dde9f0aab0f313b
Julian <jmguintu3@student.hau.edu.ph>
2025-07-07 18:45:43 +0800

    Revert "add main.java"
    This reverts commit 36ce8556b7306164c458ffb58bbb5c1d88d0add3.

--- app/src/main/java/org/example/Main.java
+++
@@ -1,8 +0,0 @@
-import bala.Window;
-
-public class Main {
-    public static void main(String[] args) {
-        Window window = Window.get(); // singleton - one instance that can be accessed globally, one game window
-        window.run();
-    }
-}
